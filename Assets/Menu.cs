83c4a4fc224866daaa3d7f7833cf42721509c407
Bill Darker <william.darker2003@gmail.com>
2021-04-27 14:10:53 +0930

    no message

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Quit : MonoBehaviour
+public class Menu : MonoBehaviour
 {
+    
     public void QuitFun()
     {
         Application.Quit();
+        Debug.Log("Quit");
     }
+    public void PlayFun()
+    {
+        if(SceneManager.GetSceneByBuildIndex(buildIndex: 0).isLoaded == false)
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+            Debug.Log("Play");
+        }
+        else
+        {
+            SceneManager.UnloadSceneAsync("Menu");
+            Debug.Log("Resume");
+        }
+    }
+
+    void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     void Update()
     {
         if (Input.GetKey("escape"))
         {
-            QuitFun();
+            SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
         }
     }
 }
