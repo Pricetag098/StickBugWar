42371de741f135f1926ab94f7000bcb9e96ecb65
Pricetag_ <jakesemail098@gmail.com>
2021-04-29 13:53:54 +0930

    Health bar and balance changes

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,11 +6,19 @@ public class Health : MonoBehaviour
 {
     public float health,maxHealth;
 
+    public Gradient hpGradient;
+    public SpriteRenderer sr;
 
+
+    private void Start()
+    {
+        sr.color = hpGradient.Evaluate(health / maxHealth);
+    }
     public void onTakeDmg(float dmg)
     {
         health -= dmg;
         if(health <= 0) { onDeath(); }
+        sr.color = hpGradient.Evaluate(health / maxHealth);
     }
     public void onDeath()
     {
